438d7dc0e8b3a9a94f95d35f25bb3e75c2d3716e
luanafernanda <lfsg.22@outlook.com>
2023-06-15 15:19:17 -0300

    exercicio com typescript criado

---
+++ ex-typescript.ts
@@ -0,0 +1,10 @@
+function multiplicacao(numeroA: number, numeroB: number):number{
+  return numeroA * numeroB
+}
+
+function saudacao(nome: string) {
+  return `Olá ${nome}`
+}
+
+console.log(multiplicacao(5,2));
+console.log(saudacao('Corretor'));
